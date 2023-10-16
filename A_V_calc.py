1a5e7fd3eeffaec2f8556cfc02d2e31e4c8cfe69
DustinH100 <145057239+DustinH100@users.noreply.github.com>
2023-10-15 22:25:37 -0400

    Add files via upload
    Consulted the internet on how to round my values and found a cleaner way to write the final outputs. Not exactly sure I'm using branches correctly.

--- A_V_calc.py
+++ A_V_calc.py
@@ -15,11 +15,11 @@ Enter Q/q to quit – select either will gracefully close the application and ca
 Enter V/v to change the calculated view or D/d for default view.
 
 	(Level 1)
-    1.	First Area/Volume* calulation
-    2.	Second Area/Volume* calculation
-    3.	Third Area/Volume* calculation
-    4.	Fourth Area/Volume* calculation
-    5.	Fifth Area/Volume* calculation
+    1.First Area/Volume* calulation
+    2.Second Area/Volume* calculation
+    3.Third Area/Volume* calculation
+    4.Fourth Area/Volume* calculation
+    5.Fifth Area/Volume* calculation
 
 *********
 
@@ -34,91 +34,101 @@ After selection at level 1, the calculator expects data to entered (use appropia
 to be included)
 
 '''
-
-#from assertions import add2
 import math
 import sys
 
-if __name__ == "__main__":
-
 def arec(leng, wid):    #Calculates the area of a rectangle
-    return print(leng*wid)
+    return leng*wid
 
 def asqr(leng):    #Calculates the area of a square
-    return print(leng**2)
+    return leng**2
 
 def acir(radi):    #Calculates the area of a circle
-    return print(math.pi*radi**2)
+    return math.pi * radi**2
 
 def acub(leng):    #Calculates the area of a cube
-    return print(6*(leng**2))
+    return 6 * leng**2
 
 def vcub(leng):    #Calculates the volume of a cube
-    return print(leng**3)
+    return leng**3
 
-while True:
-    select = input("""Enter V/v for a detailed calculation or D/d for the default view
-    or press Q/q to quit """)
+if __name__ == "__main__":
     
-    if select == "Q" or select == "q":
-        sys.exit(0)
+        while True:
+            #Asking the user how they want their calculations displayed, or to quit.
+            select = input("""Enter V/v for a detailed calculation or D/d for the default view    
+            or press Q/q to quit """)
+    
+            if select == "Q" or select == "q":
+                sys.exit(0)
         
-    if select == "D" or select == "d":
-        dcalc = int(input("""Which calculation using the designated number
-        1.Area of a Square
-        2.Area of a Rectangle
-        3.Area of a Circle
-        4.Area of a Cube
-        5.Volume of a Cube
-        """))
+            if select == "D" or select == "d":    #Asking the user which calculation to use using the default view.
+                dcalc = int(input("""Which calculation using the designated number
+                1.Area of a Square
+                2.Area of a Rectangle
+                3.Area of a Circle
+                4.Area of a Cube
+                5.Volume of a Cube
+                """))
         
-        if dcalc == 1:
-            leng = float(input("What is the length? "))
-            print(leng, "^2", "=", asqr(leng))
+                if dcalc == 1:
+                    leng = float(input("What is the length? "))
+                    result = asqr(leng)
+                    print(f"{leng}^2 = {round(result, 1)}")
             
-        if dcalc == 2:
-            leng = float(input("What is the length? "))
-            wid = float(input("What is the width? "))
-            print(leng, "*", wid, "=", arec(leng, wid))
+                if dcalc == 2:
+                    leng = float(input("What is the length? "))
+                    wid = float(input("What is the width? "))
+                    result = arec(leng, wid)
+                    print(f"{leng}*{wid} = {round(result, 1)}")
         
-        if dcalc == 3:
-            radi = float(input("What is the radius? "))
-            print("3.14", "*", radi, "=", acir(radi))
+                if dcalc == 3:
+                    radi = float(input("What is the radius? "))
+                    result = acir(radi)
+                    print(f"{math.pi}*{radi} = {round(result, 1)}")
             
-        if dcalc == 4:
-            leng = float(input("What is the length? "))
-            print("6", "*", leng, "^2", "=", acub(leng))
+                if dcalc == 4:
+                    leng = float(input("What is the length? "))
+                    result = acub(leng)
+                    print(f"6*{leng}^2 = {round(result, 1)}")
         
-        if dcalc == 5:
-            leng = float(input("What is the length? "))
-            print(leng, "^3", "=", vcub(leng))
+                if dcalc == 5:
+                    leng = float(input("What is the length? "))
+                    result = vcub(leng)
+                    print(f"{leng}^3 = {round(result, 1)}")
         
-    if select == "V" or select == "v":
-        vcalc = int(input("""Which calculation using the designated number
-        1.Area of a Square
-        2.Area of a Rectangle
-        3.Area of a Circle
-        4.Area of a Cube
-        5.Volume of a Cube
-        """))
         
-        if vcalc == 1:
-            leng = float(input("What is the length? "))
-            print(leng, "^2", "=", asqr(leng), "length^2")
+            if select == "V" or select == "v":    #Asking the user which calculation to use using the detailed view
+                vcalc = int(input("""Which calculation using the designated number
+                1.Area of a Square
+                2.Area of a Rectangle
+                3.Area of a Circle
+                4.Area of a Cube
+                5.Volume of a Cube
+                """))
+        
+                if vcalc == 1:
+                    leng = float(input("What is the length? "))
+                    result = asqr(leng)
+                    print(f"{leng}^2 = {round(result, 1)} (length^2)")
             
-        if vcalc == 2:
-            leng = float(input("What is the length? "))
-            wid = float(input("What is the width? "))
-            print(leng, "*", wid, "=", arec(leng, wid), "length * width")
+                if vcalc == 2:
+                    leng = float(input("What is the length? "))
+                    wid = float(input("What is the width? "))
+                    result = arec(leng, wid)
+                    print(f"{leng}*{wid} = {round(result, 1)} (length * width)")
         
-        if vcalc == 3:
-            radi = float(input("What is the radius? "))
-            print("3.14", "*", radi, "=", acir(radi), "pi*r^2")
+                if vcalc == 3:
+                    radi = float(input("What is the radius? "))
+                    result = acir(radi)
+                    print(f"{math.pi}*{radi} = {round(result, 1)} (pi*r^2)")
             
-        if vcalc == 4:
-            leng = float(input("What is the length? "))
-            print("6", "*", leng, "^2", "=", acub(leng), "6*length^2")
+                if vcalc == 4:
+                    leng = float(input("What is the length? "))
+                    result = acub(leng)
+                    print(f"6*{leng}^2 = {round(result, 1)} (6*length^2)")
         
-        if vcalc == 5:
-            leng = float(input("What is the length? "))
-            print(leng, "^3", "=", vcub(leng), "length^3")
+                if vcalc == 5:
+                    leng = float(input("What is the length? "))
+                    result = vcub(leng)
+                    print(f"{leng}^3 = {round(result, 1)} (length^3)")
